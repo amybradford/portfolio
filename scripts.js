4a87875e88c2460da1f01e0c4dbeb7c6640868b6
amy <amy.bradford3@gmail.com>
2021-03-24 19:10:45 +0000

    worked on projects section

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 // image slider js - used "https://flickity.metafizzy.co/#new-flickity-v2-2"
 const elem = document.querySelector('.main-carousel');
 const flkty = new Flickity( elem, {
-  // options
+  // added custom options to slider
   cellAlign: 'left',
   contain: true,
   wrapAround: true,
+  accessibility: true,
+  pageDots: false
 });
