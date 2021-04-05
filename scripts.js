1d4dfe24827b3a8dda8fb1d2a05a57350cb18df0
amy <amy.bradford3@gmail.com>
2021-04-05 22:55:37 +0100

    fade styles, scroll added

--- scripts.js
+++ scripts.js
@@ -8,3 +8,30 @@ const flkty = new Flickity( elem, {
   accessibility: true,
   pageDots: false
 });
+
+// fade-in animations - watched tutorial by kevin powell
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 1,
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      // stop looking at something once you've done your job
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
