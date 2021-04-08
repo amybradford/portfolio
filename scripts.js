d4a2f941248651b8b45fe2eee779ea41f02e4b72
amy <amy.bradford3@gmail.com>
2021-04-08 17:42:27 +0100

    changing layout of portfolio

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-// image slider js - used "https://flickity.metafizzy.co/#new-flickity-v2-2"
-const elem = document.querySelector('.main-carousel');
-const flkty = new Flickity( elem, {
-  // added custom options to slider
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  accessibility: true,
-  pageDots: false
-});
-
 // fade-in animations - watched tutorial by kevin powell
 const faders = document.querySelectorAll('.fade-in');
 
